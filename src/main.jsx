494d33cec089c70342620c95635e5f191cb507fc
soundar raj <soundarraj1222000@gmail.com>
2024-04-21 20:03:31 +0530

    version 3

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,6 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 import JobCard from './components/JobCard.jsx'
-import job from '../public/assets/'
 
 const offers=[
   {
